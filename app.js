de27db49534a39fd84d2031af97a0387369c6bc3
Dileep Singh <dileep2008apex@gmail.com>
2017-02-16 19:15:48 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,48 @@
+var express     =   require("express");
+var app         =   express();
+var bodyParser  =   require("body-parser");
+var mongoOp     =   require("./models/mongo");
+var router      =   express.Router();
+
+var mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost:27017/cars");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({"extended" : false}));
+
+router.get("/",function(req,res){
+    res.json({"error" : false,"message" : "Hello World"});
+});
+
+router.route("/models")
+    .get(function(req,res){
+        var response = {};
+        mongoOp.find({},function(err,data){
+        // Mongo command to fetch all data from collection.
+            if(err) {
+                response = {"error" : true,"message" : "Error fetching data"};
+            } else {
+                response = {"error" : false,"message" : data};
+            }
+            res.json(response);
+        });
+    });
+
+router.route("/make")
+    .get(function(req,res){
+        var response = {};
+        mongoOp.find({},function(err,data){
+        // Mongo command to fetch all data from collection.
+            if(err) {
+                response = {"error" : true,"message" : "Error fetching data"};
+            } else {
+                response = {"error" : false,"message" : data};
+            }
+            res.json(response);
+        });
+    });
+
+app.use('/',router);
+
+app.listen(3000);
+console.log("Listening to PORT 3000");
